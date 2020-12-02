0983096308958329001dbe3eac5506f1105e1cd3
AJP2511 <johnson2511@gmail.com>
2020-12-02 20:10:00 -0300

    menu_burg

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hammburger');
-const navUl = document.getElementById('menu');
-
-hamburger.addEventListener('click', () => {
-    navUl.classList.toggle('show');
-});
